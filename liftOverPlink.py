e4832d561b6e1f1ee4d3a730eb04a46c5b2451c5
minhuic <minhuic@usc.edu>
2020-05-22 22:53:37 -0700

    debug liftOverPlink.py

--- liftOverPlink.py
+++ liftOverPlink.py
@@ -13,7 +13,8 @@
 #  - to clean up the intermediary BED files to avoid confusion.
 #  - to generally be slightly more PEP compliant.
 # 
-
+# MC:
+#   - only keep autosome variants
 import sys
 import os
 import argparse
@@ -23,6 +24,7 @@ import pandas as pd
 import numpy as np
 #from string import Template
 import basic_fun
+import helper
 
 
 def die(msg):
@@ -39,8 +41,9 @@ def myopen(fn):
     return gzip.open(fn)
 
 def bim2bed(fin, fout):
-    print("Converting PLINK BIM file to UCSC BED file...")
+    sys.stderr.write("Converting PLINK BIM file to UCSC BED file...\n")
     fin = pd.read_csv(fin, sep='\s+', header=None, names=['chr', 'snp', 'genetic', 'pos', 'a1', 'a2']) # a2 a1 are ref alt
+    fin['chr'] = 'chr'+fin['chr'].astype('str')
     fin['pos0'] = fin['pos']-1
     fin[['chr', 'pos0', 'pos', 'snp']].to_csv(fout, sep='\t', index=False, header=False)
     return True
@@ -49,7 +52,7 @@ def bim2bed(fin, fout):
 LIFTED_SET = set()
 UNLIFTED_SET = set()
 def liftBed(oldBED, newBED, funlifted, chainFile, liftOverPath):
-    print("Lifting BED file...")
+    sys.stderr.write("Lifting BED file...\n")
     cmd = [liftOverPath, oldBED, chainFile, newBED, funlifted]
     basic_fun.subprocess_popen(cmd)
     #record lifted/unliftd rs
@@ -63,25 +66,27 @@ def liftBed(oldBED, newBED, funlifted, chainFile, liftOverPath):
     return True
 
 def extractBfile(oldBfile, BED, newBfile):
-    print("Extracting lifted variants from PLINK files...")
+    sys.stderr.write("Extracting lifted variants from PLINK files...\n")
     temp = tempfile.NamedTemporaryFile('w+t', delete=False)
     snp = temp.name
     for line in open(BED):
         line = line.strip().split()
         temp.write(line[3]+'\n')
     temp.close()
-    basic_fun.subprocess_popen(['plink', '--bfile', oldBfile, '--extract', snp, '--memory', args.memory, '--make-bed', 
-        '--out', newBfile])
+    basic_fun.subprocess_popen(['plink', '--bfile', oldBfile, '--extract', snp, '--memory', memory, 
+        '--keep-allele-order', '--make-bed', '--out', newBfile])
     return True
 
-def updatebim(bim, bed):
-    print("Updating SNP info in bim to new Assembly...")
-    bim = pd.read_csv(bim, sep='\s+', header=None, names=['chr', 'snp', 'genetic', 'pos', 'a1', 'a2'])
+def updatebim(bim_fn, bed, update_snpname=True):
+    sys.stderr.write("Updating SNP info in bim to new Assembly...\n")
+    bim = pd.read_csv(bim_fn, sep='\s+', header=None, names=['chr', 'snp', 'genetic', 'pos', 'a1', 'a2'])
     bim = bim[['snp', 'genetic', 'a1', 'a2']]
     bed = pd.read_csv(bed, sep='\s+', header=None, names=['chrom', 'pos0', 'pos', 'snp'])
     bed['chr'] = bed['chrom'].str.replace('^chr', '') # str 
     bed = bed.merge(bim, on='snp')
-    bed[['chr', 'snp', 'genetic', 'pos', 'a1', 'a2']].to_csv(bim, sep='\t', index=False, header=False)
+    bed[['chr', 'snp', 'genetic', 'pos', 'a1', 'a2']].to_csv(bim_fn, sep='\t', index=False, header=False)
+    if update_snpname:
+        helper.update_bim_snpname(bim_fn) # careful: are a2 and a1 ref and alt?
     return True
 
 def liftDat(fin, fout):
@@ -98,9 +103,9 @@ def liftDat(fin, fout):
 
 def makesure(result, succ_msg, fail_msg = "ERROR"):
     if result:
-        print('SUCC: ', succ_msg)
+        sys.stderr.write(f'SUCC: {succ_msg}\n')
     else:
-        print('FAIL: ', fail_msg)
+        sys.stderr.write(f'FAIL: {fail_msg}\n')
         sys.exit(2)
 
 if __name__ == '__main__':
@@ -115,7 +120,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', "--chain", dest='chainFile', required = True,
                         help='The location of the chain file to provide to ' +
                              '`liftOver`.')
-    parser.add_argument('-e', "--bin", dest='liftOverExecutable', default='liftOver',
+    parser.add_argument('-e', "--bin", dest='liftOverPath', default='liftOver',
                         help='The location of the `liftOver` executable.')
     parser.add_argument('-m', '--memory', dest='memory', default='10000',
                         help='memory for PLINK')
@@ -131,6 +136,8 @@ if __name__ == '__main__':
       sys.exit()
 
     args = parser.parse_args()
+    global memory
+    memory = args.memory 
 
     # make BED file
     oldBED = args.bfile + '.BED'
@@ -150,12 +157,12 @@ if __name__ == '__main__':
              'Extract Bfile succ')
     
     # update bim file with new position
-    makesure(updatebim(tmp_bfile+'.bim', newBed),
+    makesure(updatebim(tmp_bfile+'.bim', newBED),
              'update bim succ')
 
     # dry run PLINK to order bim file
-    basic_fun.subprocess_popen(['plink', '--bfile', tmp_bfile, '--memory', args.memory, 
-        '--make-bed', '--out', args.prefix])
+    basic_fun.subprocess_popen(['plink', '--bfile', tmp_bfile, '--memory', memory, '--keep-allele-order',
+        '--allow-extra-chr', '--chr', '1-22', '--make-bed', '--out', args.prefix])
 
     if args.datFile:
         newDat = args.prefix + '.dat'
@@ -163,7 +170,7 @@ if __name__ == '__main__':
                  'liftDat succ')
 
 
-    print("cleaning up BED files...")
-    os.remove(newBed)
-    os.remove(oldBed)
+    sys.stderr.write("cleaning up BED files...\n")
+    os.remove(newBED)
+    os.remove(oldBED)
 
